f5d4cd037794274eae271f4280aba70b0dd99a6d
杨恺 <yangkai02@qutoutiao.net>
2020-01-23 10:57:15 +0800

    add js

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,23 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
+var bodyParser = require('body-parser')
+var cors = require('cors')
+
+var json = {
+    'title': 'test json response',
+    'message': 'this is a message',
+    'time': 'now'
+}
+
 const app = express()
+app.use(cors())
+// to use json
+app.use(bodyParser.json())
+// to use url encoded values
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 app.use(express.static('dist'))
 
@@ -12,9 +28,10 @@ app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
 })
 
+const port = 8081;
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`)
 })
 
 app.get('/test', function (req, res) {
